ec8a2d58641c2ea3af06060e76652919a9f509ec
MaryAnne <maryannewanjeri4@gmail.com>
2026-07-28 11:33:51 +0300

    feat:(all files)corrected colours in style ,added a sript.js link in index file, added constants in java script

---
+++ script.js
@@ -0,0 +1,4 @@
+const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+const weekdayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+ 
